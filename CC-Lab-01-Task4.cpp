ef56002e7693ad8531eaa74dd33bb52913948041
Farhan Ashraf <53370109+mriceflame@users.noreply.github.com>
2020-10-07 23:42:47 +0500

    Update CC-Lab-01-Task4.cpp

--- CC-Lab-01-Task4.cpp
+++ CC-Lab-01-Task4.cpp
@@ -1,42 +1,30 @@
+/*Task 4: Write a program to print string from text file in forward and reverse order.*/
 #include<iostream>
 #include<string>
 #include<fstream>
 using namespace std;
-int main() {
-	string s, s2, cs, cs2; char c;
-	cout << "Enter the first string: "; cin >> s;
-	cout << "Enter the second String: "; cin >> s2;
-	fstream obj, obj2;
-	obj.open("file.txt", fstream::out);
-	obj << s;
-	obj.close();
-	obj2.open("file2.txt", fstream::out);
-	obj2 << s2;
-	obj2.close();
-	obj.open("file.txt");
-	if (obj.is_open())
+int main()
+{
+	string string,s,sup; char c;
+	cout << "Enter the String: "; cin >> string;
+	fstream file;
+	file.open("File.txt", fstream::out);
+	file << string;
+	file.close();
+	file.open("file.txt",fstream::in);
+	if (file.is_open())
 	{
-		int i = 0;
-		while (!obj.eof())
+		while (!file.eof())
 		{
-			obj.get(c);
-			cs = cs + c;
+			file.get(c);
+			if (!file.fail())
+			s += c;
 		}
 	}
-	obj.close();
+	cout << "The string in Forward Order: " << string << endl;
+	cout << "The String in Reverse Order: ";
+	for (int i = s.length()-1; i >-1; i--) {
+		cout << s[i];
+	}
 
-	obj2.open("file2.txt");
-	c = '\0';
-	if (obj2.is_open())
-	{
-		int i = 0;
-		while (!obj2.eof())
-		{
-			obj2.get(c);
-			cs2 = cs2 + c;
-		}
-	}
-	if (cs == cs2) {
-		cout << "Both strings are same" << endl;
-	}
-	else cout << "Both are not" << endl;
+	return 0;
